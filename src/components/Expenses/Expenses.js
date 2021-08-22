16d7f8b78e1cfd84243a0499cdd00d2c4b66b2a1
Addie <addie@aiii.ai>
2021-08-23 02:31:14 +0800

    [Simple-Expenses-Tracker]
    1. Modify part:
    Create one simple expense tracker with React
    
    2. Purpose:
    Self-practice
    
    3. Test:
    N/A

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
+
+import './Expenses.css'
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (yearData) => {
+    setFilteredYear(yearData)
+  }
+
+  const filteredExpenses = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear
+  })
+
+  // let expensesContent = <p>No expenses found</p>
+
+  // if (filteredExpenses.length > 0) {
+  //   expensesContent = filteredExpenses.map((item) => (
+  //     <ExpenseItem
+  //       title={item.title}
+  //       amount={item.amount}
+  //       date={item.date}
+  //       key={item.id}
+  //     />
+  //   ))
+  // }
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      <ExpensesList item={filteredExpenses} />
+
+      {/* {expensesContent} */}
+      {/* {filteredExpenses.length === 0 ? (
+        <p>No expenses found</p>
+      ) : (
+        filteredExpenses.map((item) => (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+            key={item.id}
+          />
+        ))
+      )} */}
+    </Card>
+  )
+}
+
+export default Expenses
